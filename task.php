01934a3420b2a4be8c8d1a12b8006973ba3125da
GON <hutabasuzukiryu@live.jp>
2019-06-17 11:11:09 +0900

    init commit

---
+++ task.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>タスク一覧</title>
+    </head>
+    <body>
+        <table border="1">
+<?php
+$conn = new mysqli("localhost", "s1811424", "s1811424new", "s1811424");
+$conn->set_charset("utf8");
+$sql = "SELECT * FROM task";
+$res = $conn->query($sql);
+print("<tr>");
+for ($i=0; $i < $res->field_count; $i++) { 
+    print("<td>" . $res->fetch_field_direct($i)->name . "</td>");
+}
+print("</tr>");
+
+while ($row = $res->fetch_array()) {
+    print("<tr>");
+    for ($i=0; $i < $res->field_count; $i++) { 
+        print("<td>" . $row[$i] . "</td>");
+    }
+    print("</tr>");
+}
+?>
+        </table>
+    </body>
+</html>
